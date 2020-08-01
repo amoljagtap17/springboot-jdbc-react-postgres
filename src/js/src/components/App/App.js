56efa630b7498b6cd7ca1621850638f4b16248a7
Amol Jagtap <amoljagtap17@gmail.com>
2020-08-02 00:44:46 +0530

    SPRINGBOOT-JDBC-REACT-POSTGRES: Add Spinner and Table components

--- src/js/src/components/App/App.js
+++ src/js/src/components/App/App.js
@@ -1,51 +1,84 @@
 import React, { useState, useEffect } from 'react'
 import fetch from 'unfetch'
-import { Table } from 'antd'
+import { Table, Avatar, Spin } from 'antd'
+import { Container } from 'components/Layout'
+
+const columns = [
+  {
+    title: '',
+    key: 'avatar',
+    render: (text, student) => (
+      <Avatar size="large">
+        {`${student.firstName.charAt(0).toUpperCase()}${student.lastName
+          .charAt(0)
+          .toUpperCase()}`}
+      </Avatar>
+    )
+  },
+  {
+    title: 'Student Id',
+    dataIndex: 'studentId',
+    key: 'studentId'
+  },
+  {
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName'
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName'
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email'
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender'
+  }
+]
 
 export const App = () => {
   const [students, setStudents] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const getAllStudents = async () => {
+      setLoading(true)
+
       const res = await fetch('/students')
       const students = await res.json()
 
       setStudents(students)
+      setLoading(false)
     }
 
     getAllStudents()
   }, [])
 
-  if (students && students.length) {
-    const columns = [
-      {
-        title: 'Student Id',
-        dataIndex: 'studentId',
-        key: 'studentId'
-      },
-      {
-        title: 'First Name',
-        dataIndex: 'firstName',
-        key: 'firstName'
-      },
-      {
-        title: 'Last Name',
-        dataIndex: 'lastName',
-        key: 'lastName'
-      },
-      {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email'
-      },
-      {
-        title: 'Gender',
-        dataIndex: 'gender',
-        key: 'gender'
-      }
-    ]
+  if (loading) {
+    return (
+      <Container>
+        <Spin size="large" tip="Loading..." />
+      </Container>
+    )
+  }
 
-    return <Table dataSource={students} columns={columns} rowKey="studentId" />
+  if (students && students.length) {
+    return (
+      <Container>
+        <Table
+          dataSource={students}
+          columns={columns}
+          pagination={false}
+          rowKey="studentId"
+        />
+      </Container>
+    )
   }
 
   return <h1>No Students found!</h1>
---
+++ src/js/src/components/Layout/Container.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Container = ({ children }) => (
+  <div style={{ width: '1400px', margin: '0 auto', textAlign: 'center' }}>
+    {children}
+  </div>
+)
