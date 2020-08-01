75162489f727404c06e413a848f0049e8b7c7450
Amol Jagtap <amoljagtap17@gmail.com>
2020-08-02 00:27:10 +0530

    SPRINGBOOT-JDBC-REACT-POSTGRES: Add 'antd' Table component to display Students data

--- src/js/src/components/App/App.js
+++ src/js/src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import fetch from 'unfetch'
+import { Table } from 'antd'
 
 export const App = () => {
   const [students, setStudents] = useState([])
@@ -16,15 +17,35 @@ export const App = () => {
   }, [])
 
   if (students && students.length) {
-    return students.map((student) => (
-      <div key={student.studentId}>
-        <h2>{student.studentId}</h2>
-        <p>{student.firstName}</p>
-        <p>{student.lastName}</p>
-        <p>{student.gender}</p>
-        <p>{student.email}</p>
-      </div>
-    ))
+    const columns = [
+      {
+        title: 'Student Id',
+        dataIndex: 'studentId',
+        key: 'studentId'
+      },
+      {
+        title: 'First Name',
+        dataIndex: 'firstName',
+        key: 'firstName'
+      },
+      {
+        title: 'Last Name',
+        dataIndex: 'lastName',
+        key: 'lastName'
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender'
+      }
+    ]
+
+    return <Table dataSource={students} columns={columns} rowKey="studentId" />
   }
 
   return <h1>No Students found!</h1>
