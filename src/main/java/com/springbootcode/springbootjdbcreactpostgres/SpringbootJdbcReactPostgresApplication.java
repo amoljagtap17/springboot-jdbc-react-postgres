49907765b0f250b7193ce98d4053d9f360f5c4b4
Amol Jagtap <amoljagtap17@gmail.com>
2020-08-01 22:06:48 +0530

    SPRINGBOOT-JDBC-REACT-POSTGRES: Init Spring Boot

---
+++ src/main/java/com/springbootcode/springbootjdbcreactpostgres/SpringbootJdbcReactPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.springbootcode.springbootjdbcreactpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcReactPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcReactPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootcode/springbootjdbcreactpostgres/SpringbootJdbcReactPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcode.springbootjdbcreactpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcReactPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
