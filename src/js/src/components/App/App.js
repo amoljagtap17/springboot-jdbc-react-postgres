abb6e8753e2c941548d07513a3bed760b1e79080
Amol Jagtap <amoljagtap17@gmail.com>
2020-08-02 00:17:01 +0530

    SPRINGBOOT-JDBC-REACT-POSTGRES: Display data from API on UI

--- src/js/src/components/App/App.js
+++ src/js/src/components/App/App.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from 'react'
-import { getAllStudents } from 'api/students'
+import React, { useState, useEffect } from 'react'
+import fetch from 'unfetch'
 
 export const App = () => {
+  const [students, setStudents] = useState([])
+
   useEffect(() => {
+    const getAllStudents = async () => {
+      const res = await fetch('/students')
+      const students = await res.json()
+
+      setStudents(students)
+    }
+
     getAllStudents()
-      .then((res) => res.json())
-      .then((students) => {
-        console.log('students', students)
-      })
   }, [])
 
-  return <div>App</div>
+  if (students && students.length) {
+    return students.map((student) => (
+      <div key={student.studentId}>
+        <h2>{student.studentId}</h2>
+        <p>{student.firstName}</p>
+        <p>{student.lastName}</p>
+        <p>{student.gender}</p>
+        <p>{student.email}</p>
+      </div>
+    ))
+  }
+
+  return <h1>No Students found!</h1>
 }
